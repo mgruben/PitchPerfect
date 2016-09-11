0ddd0f4afd744027d54c7d0de067cc01efa9dc4f
Michael <GrubenM@gmail.com>
2016-09-11 14:14:04 -0500

    Rename ViewController to RecordAudioViewController

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  PitchPerfect
 //
 //  Created by Michael on 8/28/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
