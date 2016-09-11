9fcf38eb79cb13ab2a0e215e0b0bb70f1c51c074
Michael <GrubenM@gmail.com>
2016-09-11 14:49:59 -0500

    Add ability to save recorded audio as a file
    Unfortunately, there are problems with nil values here which others on the Udacity Forums have noted, and which have not received any reply or assistance.
    
    Accordingly, this commit breaks the app, until a fix can be founds.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var recordingLabel: UILabel!
-
+    var audioRecorder:AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,12 +28,28 @@ class ViewController: UIViewController {
         recordingLabel.text = "Recording..."
         recordButton.enabled = false
         stopRecordingButton.enabled = true
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     @IBAction func stopRecordingAudio(sender: AnyObject) {
         print("StopRecording button pressed")
         recordButton.enabled = true
         stopRecordingButton.enabled = false
         recordingLabel.text = "Tap to Record"
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
     override func viewWillAppear(animated: Bool) {
         stopRecordingButton.enabled = false
